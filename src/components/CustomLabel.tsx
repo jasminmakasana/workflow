fd7d386d0f81cd2d56a05277ef418f518d202cb6
Sagar <sagardevelops15@gmail.com>
2024-03-01 09:53:35 +0530

    Workflow lib build initial commit

---
+++ src/components/CustomLabel.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+export class CustomLabel extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+  }
+  onChange = (e: any) => {
+    if (this.props.onChange) {
+      this.props.onChange(e);
+    }
+  };
+  render() {
+    const {
+      containerClass,
+      labelClass,
+      label,
+      htmlFor,
+      value,
+      isValid,
+      message,
+      notice,
+    } = this.props;
+    return (
+      <div className={containerClass}>
+        <label className={labelClass} htmlFor={htmlFor}>
+          {label}: {value}
+        </label>
+        {!isValid && <div className="invalid-feedback">{message}</div>}
+        <span>{notice}</span>
+      </div>
+    );
+  }
+}
